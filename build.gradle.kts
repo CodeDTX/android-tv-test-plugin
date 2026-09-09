4d91e907be117f5daea21108a06aaf9aa09beb82
kshitij-codedtx <kshitij.r@gmail.com>
2026-09-09 10:53:27 +0530

    Add full plugin source, build files, and CI workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    id("com.android.library")              version "8.7.3"  apply false
-    id("org.jetbrains.kotlin.android")     version "1.9.23" apply false
-    id("java-gradle-plugin")               apply false
-    `maven-publish`
+    id("com.android.library")          version "8.7.3"  apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
